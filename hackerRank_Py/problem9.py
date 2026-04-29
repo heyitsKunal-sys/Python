e0f8d234db1044adb2ce0888d632ff51cd34c044
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-04-29 20:33:56 +0530

    commit

--- hackerRank_Py/problem9.py
+++ hackerRank_Py/problem9.py
@@ -7,9 +7,9 @@
 
 # 5
 n = int(input())
-a = list(map(int,input().split()))
+arr = list(map(int,input().split()))
 
-score = sorted(list(set(a)))
+score = sorted(list(set(arr)))
 print(score[-2])
 
 # input(): Reads the entire line of text you typed as a single string (e.g., "10 20 30").
@@ -17,7 +17,7 @@ print(score[-2])
 # map(int, ...): This is the "converter." It takes every individual string in that list and applies the int() function to it, turning them into real integers so you can do math with them.
 # list(...): The map function creates an "iterator" (a lazy object). Wrapping it in list() forces it to actually generate all those integers and store them in a standard Python list.
 
-# set(a): A set cannot have duplicate items. By passing your list a into set(), Python instantly deletes any repeating numbers.
+# set(arr): A set cannot have duplicate items. By passing your list a into set(), Python instantly deletes any repeating numbers.
 # Example: [10, 5, 10, 8] becomes {5, 8, 10}.
 # list(...): This converts the set back into a list format so you can manipulate it more easily.
 # sorted(...): This takes that list and puts the numbers in order from smallest to largest.
